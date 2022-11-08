c18f45160ae24957817a6f42abf2961a6cb565fe
AkashNadar <Akashnadar14@gmail.com>
2022-11-08 16:14:12 +0530

    changes done on the Patient Problem Service

--- BackEnd/PatientProblemService/src/main/java/com/idfc/model/PatientProblem.java
+++ BackEnd/PatientProblemService/src/main/java/com/idfc/model/PatientProblem.java
@@ -7,48 +7,47 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import org.hibernate.annotations.ColumnDefault;
 
 
 @Entity
-@Table(name = "users")
-public class Patient {
+@Table(name = "patient_problem_details")
+public class PatientProblem {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private int problemId;
+	private long id;
 	
 	@Column(nullable = false)
-	private String patientId;
+	private long patientId;
 	
 	@Column(nullable = false)
-	private String sympoms;
+	private String symptoms;
 	
 	@Column(nullable = false)
 	private String pastMedHist;
 	
-	public int getProblemId() {
-		return problemId;
+	public long getProblemId() {
+		return id;
 	}
 
 	public void setProblemId(int problemId) {
-		this.problemId = problemId;
+		this.id = problemId;
 	}
 
-	public String getPatientId() {
+	public long getPatientId() {
 		return patientId;
 	}
 
-	public void setPatientId(String patientId) {
+	public void setPatientId(long patientId) {
 		this.patientId = patientId;
 	}
 
 	public String getSympoms() {
-		return sympoms;
+		return symptoms;
 	}
 
 	public void setSympoms(String sympoms) {
-		this.sympoms = sympoms;
+		this.symptoms = sympoms;
 	}
 
 	public String getPastMedHist() {
@@ -59,7 +58,18 @@ public class Patient {
 		this.pastMedHist = pastMedHist;
 	}
 
+	public PatientProblem() {
+		super();
+		//TODO Auto-generated constructor stub
+	}
 
+	public PatientProblem(long id, long patientId, String sympoms, String pastMedHist) {
+		super();
+		this.id = id;
+		this.patientId = patientId;
+		this.symptoms = sympoms;
+		this.pastMedHist = pastMedHist;
+	}
 	
 	
 }
