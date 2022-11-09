8f5650c47a49fc7b3a62a7899d5282d263335106
Dhanush-K-dk <71658166+Dhanush-K-dk@users.noreply.github.com>
2022-11-09 13:32:14 +0530

    Delete BackEnd/PrescriptionDiagnosisService directory

--- BackEnd/PrescriptionDiagnosisService/src/main/java/com/dk/dao/diagRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dk.dao;
-
-import com.dk.model.Diagnosis;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface diagRepository extends JpaRepository<Diagnosis, Long> {
-
-}
